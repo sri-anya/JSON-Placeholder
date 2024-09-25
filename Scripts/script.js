d4a5e7d4ae49f5837a4aaffdeab3565a4be6b063
Aastha Saxena <aasthasaxena100@gmail.com>
2024-09-25 12:37:50 -0400

    post not working

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,35 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetchTodos();
-
-    document.getElementById('add-todo').addEventListener('click', addTodo);
-});
-
-function fetchTodos() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            displayTodos(data);
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
-}
 
 function displayTodos(todos) {
     const todoList = document.getElementById('todo-list');
     todoList.innerHTML = ''; // Clear existing todos
 
-    todos.forEach(todo => {
+    todos.forEach((todo) => {
         const li = document.createElement('li');
         li.textContent = `${todo.id}: ${todo.title} - ${todo.completed ? 'Completed' : 'Not completed'}`;
         todoList.appendChild(li);
     });
 }
+
+function fetchTodos() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            displayTodos(data);
+        })
+        .catch((error) => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
 
 function addTodo() {
     const todoInput = document.getElementById('new-todo');
@@ -46,17 +42,17 @@ function addTodo() {
         fetch('https://jsonplaceholder.typicode.com/todos', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newTodo),
+            body: JSON.stringify(newTodo)
         })
-        .then(response => {
+        .then((response) => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
             // Display the new todo at the top of the list
             const li = document.createElement('li');
             li.textContent = `${data.id}: ${data.title} - Not completed`;
@@ -66,10 +62,15 @@ function addTodo() {
             // Clear the input field
             todoInput.value = '';
         })
-        .catch(error => {
+        .catch((error) => {
             console.error('There was a problem with the fetch operation:', error);
         });
     } else {
         alert('Please enter a todo!');
     }
 }
+document.addEventListener('DOMContentLoaded', () => {
+    fetchTodos();
+
+    document.getElementById('add-todo').addEventListener('click', addTodo);
+});
